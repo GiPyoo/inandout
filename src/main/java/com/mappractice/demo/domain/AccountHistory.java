65e7ea29b63738e8b702963ee01380a04ed2fb3b
정민석 <dadadamarine@gmail.com>
2019-06-27 09:48:29 +0900

    fix : 계좌 entity 수정

--- src/main/java/com/mappractice/demo/domain/AccountHistory.java
+++ src/main/java/com/mappractice/demo/domain/AccountHistory.java
@@ -1,6 +1,8 @@
 package com.mappractice.demo.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,26 +11,29 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AccountHistory {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String account;
 
-    @OneToOne
-    @Column
-    private Category category;
+    @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_history_to_virtual"))
+    private VirtualAccount virtualAccount;
 
     @Column
-    private LocalDateTime date;
-
-    @Column
-    private String client;
+    private LocalDateTime createdAt = LocalDateTime.now();
 
     @Column
     private int transaction;
 
     @Column
-    private Long money;
+    private Long deposit;
+
+    @Column
+    private Long withdraw;
 
     @Column
     private Long amount;
