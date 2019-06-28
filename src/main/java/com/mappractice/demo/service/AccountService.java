3697dbf9feaedf57bd1ac72724361c55e36835c8
Ilisha <40922963+Ilisha29@users.noreply.github.com>
2019-06-28 23:22:27 +0900

    feat

--- src/main/java/com/mappractice/demo/service/AccountService.java
+++ src/main/java/com/mappractice/demo/service/AccountService.java
@@ -1,4 +1,49 @@
 package com.mappractice.demo.service;
 
+import com.mappractice.demo.domain.AccountHistory;
+import com.mappractice.demo.domain.AccountHistoryRepository;
+import com.mappractice.demo.domain.VirtualAccount;
+import com.mappractice.demo.domain.VirtualAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+// 구현 미완료!!!!!
+@Service
 public class AccountService {
+
+    @Autowired
+    VirtualAccountRepository virtualAccountRepository;
+
+    @Autowired
+    AccountHistoryRepository accountHistoryRepository;
+
+    public void paymentProcess(VirtualAccount from, Long money) {
+        withdrawalProcess(from, money);
+    }
+
+    //가상계좌간의 이체
+    public void transferProcess(VirtualAccount from, VirtualAccount to, Long money) {
+        withdrawalProcess(from, money);
+        depositProcess(to, money);
+    }
+
+    private void withdrawalProcess(VirtualAccount from, Long money) {
+        long virtualAccountAmount = from.getAmount();
+        virtualAccountAmount -= money;
+        from.setAmount(virtualAccountAmount);
+        makeProcessHistory(from);
+    }
+
+    private void depositProcess(VirtualAccount to, Long money) {
+        long virtual_account_amount = to.getAmount();
+        virtual_account_amount += money;
+        to.setAmount(virtual_account_amount);
+        makeProcessHistory(to);
+    }
+
+    private void makeProcessHistory(VirtualAccount virtualAccount) {
+        virtualAccountRepository.save(virtualAccount);
+    }
+
+   // private void make
+
 }
--- src/test/java/com/mappractice/demo/utilsTest/DateUtilsTest.java
+++ src/test/java/com/mappractice/demo/utilsTest/DateUtilsTest.java
@@ -1,4 +1,16 @@
 package com.mappractice.demo.utilsTest;
 
+import com.mappractice.demo.utils.DateUtils;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DateUtilsTest {
+    @Test
+    public void checkNewModificationTest(){
+        String LatestLoginTimeOfUser = "20190628095011";
+        String LatestModificationTimeOfTransactionHistory = "20190628095200";
+        assertEquals(true, DateUtils.checkNewModification(LatestLoginTimeOfUser,LatestModificationTimeOfTransactionHistory));
+    }
+
 }
