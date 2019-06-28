7ab5591e8e57d4b8d80897566bb63396d4239616
정민석 <dadadamarine@gmail.com>
2019-06-28 21:32:32 +0900

    feature : get api 테스팅 완료(#54)

--- src/main/java/com/mappractice/demo/utils/RequestGenerator.java
+++ src/main/java/com/mappractice/demo/utils/RequestGenerator.java
@@ -1,28 +1,41 @@
 package com.mappractice.demo.utils;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import com.mappractice.demo.hackaton.dto.TransactionHistoryResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 public class RequestGenerator {
-    public static String getJson(String url) throws Exception{
+//    public static String getJson(String url) throws Exception {
+//        URL obj = new URL(url);
+//        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+//        con.setRequestMethod("GET");
+//        con.setRequestProperty("Accept", "application/json");
+//        System.out.println("\nSending 'GET' request to URL : " + url);
+//        BufferedReader in = new BufferedReader(
+//                new InputStreamReader(con.getInputStream()));
+//        String inputLine;
+//        StringBuffer response = new StringBuffer();
+//        while ((inputLine = in.readLine()) != null) {
+//            response.append(inputLine);
+//        }
+//        in.close();
+//
+//        int responseCode = con.getResponseCode();
+//        System.out.println("Response Code : " + responseCode);
+//
+//        return response.toString();
+//    }
 
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("User-Agent", "Mozilla/5.0");
-        int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'GET' request to URL : " + url);
-        System.out.println("Response Code : " + responseCode);
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-        return response.toString();
+    public static TransactionHistoryResponseDTO getJsonByRestTemplate(String url) {
+        RestTemplate restTemplate = new RestTemplate();
+        URI uri = UriComponentsBuilder.fromHttpUrl(url)
+                .build().toUri();
+
+        ResponseEntity<TransactionHistoryResponseDTO> response = restTemplate.getForEntity(uri, TransactionHistoryResponseDTO.class);
+        return response.getBody();
+
     }
 }
