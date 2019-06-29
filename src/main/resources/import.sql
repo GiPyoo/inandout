24f9f085ef4b1f07d545e26274de858818f1c516
Bellroute <kjkun7631@gmail.com>
2019-06-30 08:17:24 +0900

    chore

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -33,5 +33,8 @@ INSERT INTO CATEGORY (id, name) VALUES (6, '쇼핑')
 INSERT INTO CATEGORY (id, name) VALUES (7, '교육')
 
 INSERT INTO virtual_account (id, category_id, user_id,amount, name) VALUES (1, 0, 1, 0, '기타');
-INSERT INTO virtual_account (id, category_id, user_id,amount, name) VALUES (2, 1, 1, 200000,'점심밥값');
+INSERT INTO virtual_account (id, category_id, user_id,amount, name) VALUES (2, 1, 1, 200000,'밥값');
+INSERT INTO virtual_account (id, category_id, user_id,amount, name) VALUES (3, 2, 1, 100000,'10만원 한도');
+INSERT INTO virtual_account (id, category_id, user_id,amount, name) VALUES (4, 5, 1, 100000,'걸어다니기');
+
 
