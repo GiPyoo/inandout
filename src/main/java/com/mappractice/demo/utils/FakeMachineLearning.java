3a3fe65acba85a9264e20ecedae3034ec724564c
jbj616 <jbj616@gmail.com>
2019-06-29 13:56:30 +0900

    요소 추가

--- src/main/java/com/mappractice/demo/utils/FakeMachineLearning.java
+++ src/main/java/com/mappractice/demo/utils/FakeMachineLearning.java
@@ -22,15 +22,20 @@ public class FakeMachineLearning {
 
         machine.put("gs25", 4L);
         machine.put("cu", 4L);
+        machine.put("미니스톱", 4L);
+        machine.put("이마트", 4L);
 
         machine.put("sk텔레콤", 5L);
         machine.put("LGU+", 5L);
+        machine.put("KT", 5L);
 
         machine.put("11번가", 6L);
         machine.put("타몬", 6L);
         machine.put("쿠팡", 6L);
+        machine.put("위메프", 6L);
 
         machine.put("메가스터디", 7L);
+        machine.put("이투스", 7L);
     }
 
     public static long findMachineLearning(String key){
