1e3b84b04ed06d585afc70e07cf462615734b533
jbj616 <jbj616@gmail.com>
2019-06-29 13:51:46 +0900

    feat:create FakeMachineLearning

--- src/main/java/com/mappractice/demo/utils/FakeMachineLearning.java
+++ src/main/java/com/mappractice/demo/utils/FakeMachineLearning.java
@@ -6,20 +6,40 @@ public class FakeMachineLearning {
 
     public static HashMap<String, Long> machine = new HashMap<>();
 
-    FakeMachineLearning(){
-        machine.put("홍길동", 0L);
-        machine.put("홍길동", 0L);
-        machine.put("홍길동", 0L);
-        machine.put("홍길동", 0L);
-        machine.put("홍길동", 0L);
-        machine.put("홍길동", 0L);
-        machine.put("홍길동", 0L);
-        machine.put("홍길동", 0L);
-        machine.put("홍길동", 0L);
-        machine.put("홍길동", 0L);
-        machine.put("홍길동", 0L);
-        machine.put("홍길동", 0L);
-        machine.put("홍길동", 0L);
-        machine.put("홍길동", 0L);
+    public static void setting(){
+        machine.put("아몰랑", 0L);
+
+        machine.put("BBQ", 1L);
+        machine.put("BHC", 1L);
+        machine.put("도미노피자", 1L);
+        machine.put("PizzaHut", 1L);
+
+        machine.put("메가박스", 2L);
+        machine.put("CGV", 2L);
+        machine.put("롯데시네마", 2L);
+
+        machine.put("스타벅스", 3L);
+
+        machine.put("gs25", 4L);
+        machine.put("cu", 4L);
+
+        machine.put("sk텔레콤", 5L);
+        machine.put("LGU+", 5L);
+
+        machine.put("11번가", 6L);
+        machine.put("타몬", 6L);
+        machine.put("쿠팡", 6L);
+
+        machine.put("메가스터디", 7L);
+    }
+
+    public static long findMachineLearning(String key){
+        if(machine.isEmpty()){
+            setting();
+        }
+        if(machine.containsKey(key)){
+            return machine.get(key);
+        }
+        return -1;
     }
 }
---
+++ src/test/java/com/mappractice/demo/utils/FakeMachineLearningTest.java
@@ -0,0 +1,14 @@
+package com.mappractice.demo.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FakeMachineLearningTest {
+
+
+    @Test
+    public void findMachineLearning() {
+        System.out.println(FakeMachineLearning.findMachineLearning("메가스터디"));
+    }
+}
