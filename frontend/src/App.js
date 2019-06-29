1cf11818ade5cd651381bbc43a6d7eeee2da2ab4
Asua Hostin <a01054394111@gmail.com>
2019-06-29 21:51:54 +0900

    feat : css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,45 @@
-import React, {Component} from 'react';
-import {Route, BrowserRouter} from 'react-router-dom';
-import {Payment, Home, CheckPayment , SignIn, SignUp, ImageStore} from './pages';
+import React, { Component } from "react";
+import { Route, BrowserRouter } from "react-router-dom";
+import {
+  Payment,
+  Home,
+  CheckPayment,
+  SignIn,
+  SignUp,
+  ImageStore
+} from "./pages";
 
 class App extends Component {
-  
   constructor(props) {
     super(props);
     var data = {
-      name : '',
-      price : ''
-    }
-    this.state = {PaymentData: data};
+      name: "",
+      price: ""
+    };
+    this.state = { PaymentData: data };
     console.log(data);
   }
 
-  PaymentCallBack = (dataFromChild) => {
+  PaymentCallBack = dataFromChild => {
     this.setState({
-      PaymentData : dataFromChild
-    })
-  }
+      PaymentData: dataFromChild
+    });
+  };
 
-  render(){
+  render() {
     return (
       <BrowserRouter>
-        <Route exact path ="/payment" render = {() => <Payment callbackFromParent = {this.PaymentCallBack} />}/>
-        <Route exact path ="/payment/result" render ={() => <CheckPayment data = {this.state.PaymentData} />}/>
-        <Route exact path ="/home" component={Home} />
+        <Route
+          exact
+          path="/payment"
+          render={() => <Payment callbackFromParent={this.PaymentCallBack} />}
+        />
+        <Route
+          exact
+          path="/payment/result"
+          render={() => <CheckPayment data={this.state.PaymentData} />}
+        />
+        <Route exact path="/home" component={Home} />
         <Route exact path="/login" component={SignIn} />
         <Route exact path="/signUp" component={SignUp} />
         <Route exact path="/AccountDetail" component={SignUp} />
